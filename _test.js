a73bb813981abd6f561783a895c05e698af9b9b8
Subhajit Sahu <wolfram77@gmail.com>
2019-04-05 15:32:49 +0530

    add test

--- _test.js
+++ _test.js
@@ -2,19 +2,27 @@ const ip = require('ip');
 const net = require('./');
 
 
-const ITERATIONS = 10000;
+async function address() {
+  var I = 10000;
+  var start = process.hrtime.bigint();
+  for(var i=0; i<I; i++)
+    var {address} = net.address();
+  var end = process.hrtime.bigint();
+  console.log('net.address() took', (end-start)/BigInt(1000*I), 'us');
+}
 
+async function freePort() {
+  var I = 1000, _ports = [];
+  var start = process.hrtime.bigint();
+  for(var i=0; i<I; i++)
+    _ports[i] = net.freePort();
+  var ports = await Promise.all(_ports);
+  var end = process.hrtime.bigint();
+  console.log('net.freePort() took', (end-start)/BigInt(1000*I), 'us');
+}
 
-var start = process.hrtime.bigint();
-for(var i=0; i<ITERATIONS; i++)
-  var {address} = net.address();
-var end = process.hrtime.bigint();
-console.log('net.address() took', end-start);
-
-
-var start = process.hrtime.bigint();
-for(var i=0; i<ITERATIONS; i++)
-  var {address} = ip.address();
-var end = process.hrtime.bigint();
-console.log('ip.address() took', end-start);
-
+async function test() {
+  await address();
+  await freePort();
+}
+test();
